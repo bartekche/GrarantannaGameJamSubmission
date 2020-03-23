62a46f14d137a0e6d7f67b3e692e48dab2b2b70b
polalab <labedzka.pola@gmail.com>
2020-03-23 11:06:28 +0100

    falling circle

---
+++ brachu/Assets/Scripts/missile.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class missile : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision)
+	{
+		// Put a particle effect here
+		Destroy(gameObject);
+	}
+}
+

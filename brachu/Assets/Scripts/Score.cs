f9e21f5f6f894d1e446beba96419cdc1c39d0a71
Sudoxo <44445493+Sudoxo@users.noreply.github.com>
2020-03-24 20:19:54 +0100

    Import font, add textscore

---
+++ brachu/Assets/Scripts/Score.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public static int score;
+    Text scoretext;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        scoretext = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score = Mathf.RoundToInt(Time.time * 10);
+        scoretext.text = score.ToString();
+
+    }
+}

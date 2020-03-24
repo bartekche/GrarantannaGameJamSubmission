d26ad69d147164db4c9c5fa147267eb6392a8dff
bartekche <bartekche@gmail.com>
2020-03-24 01:30:43 +0100

    Added skull spawner + skull movement

---
+++ brachu/Assets/Scripts/SkullMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkullMovement : MonoBehaviour
+{
+    public float velocity = -0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 p = transform.position;
+        p.x -= velocity;
+        transform.position = p;
+
+    }
+}
---
+++ brachu/Assets/Scripts/SkullSpawn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkullSpawn : MonoBehaviour
+{
+    public GameObject prefab;
+    public float spawnRate = 2f;
+    float nextSpawn;
+    // Start is called before the first frame update
+    // Update is called once per frame
+    void Update()
+    {
+     if(Time.time > nextSpawn)
+        {
+            GameObject skull = Instantiate(prefab, transform.position, Quaternion.identity);
+            nextSpawn = Time.time + spawnRate;
+        }
+        
+    }
+}

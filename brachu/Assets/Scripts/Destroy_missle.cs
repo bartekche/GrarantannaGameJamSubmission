324df962790199d52830d65bf901fd9b40cc0de6
polalab <labedzka.pola@gmail.com>
2020-03-24 19:39:21 +0100

    Add Boundary to destroy skulls

---
+++ brachu/Assets/Scripts/Destroy_missle.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy_missle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    void OnCollisionEnter2D(Collision2D collision)
+	{
+		// Put a particle effect here
+		Destroy(collision.gameObject);
+	}
+}

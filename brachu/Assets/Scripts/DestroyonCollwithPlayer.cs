85b90a987b1c799e72916b7a2fb22bffe45f4759
polalab <labedzka.pola@gmail.com>
2020-03-24 22:24:44 +0100

    Destroy missles hit by player

---
+++ brachu/Assets/Scripts/DestroyonCollwithPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyonCollwithPlayer : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D col)
+	{
+		// Put points/ end of game
+        
+        if(col.gameObject.tag != "ground" && col.gameObject.layer != 9 )
+        {
+            Destroy(col.gameObject);
+        }
+        
+        
+		    
+	}
+}

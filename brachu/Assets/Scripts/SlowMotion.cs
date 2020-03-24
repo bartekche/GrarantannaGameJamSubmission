27ca46483473e751f5610d3ec5fe9adb9a09d43a
Sudoxo <44445493+Sudoxo@users.noreply.github.com>
2020-03-24 03:24:51 +0100

    Add SlowMotion [NEED FIX]
    Żeby włączyć slowmotion trzeba odhaczyć na scenie objekt "TimeController", obecnie slowmo włącza się kiedy prędkość ludzika jest więszka od 0. Jest problem bo nie da się skakać z miejsca.

---
+++ brachu/Assets/Scripts/SlowMotion.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowMotion : MonoBehaviour
+{
+    private float slowmo = 0.1f;
+    private float normaltime = 1.0f;
+    private bool slow = false;
+
+    public GameObject player;
+    private Rigidbody2D rb;
+    private void Update()
+    {
+        rb = player.GetComponent<Rigidbody2D>();
+        if ( (Mathf.Abs(rb.velocity.x) > 0.01f ) || (Mathf.Abs(rb.velocity.y) > 0.01f) )
+        {
+            if(slow)
+            {
+                Time.timeScale = normaltime;
+                Time.fixedDeltaTime = 0.02f * Time.timeScale;
+                slow = false;
+            }
+        }
+        else
+        {
+            Time.timeScale = slowmo;
+            Time.fixedDeltaTime = 0.02f * Time.timeScale;
+            slow = true;
+        }
+    }
+}

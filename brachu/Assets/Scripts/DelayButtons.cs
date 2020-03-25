5c2dcde6b0c3b74a961a4636999ae5e4e6e286ec
bartekche <bartekche@gmail.com>
2020-03-26 00:05:54 +0100

    Tutorial screen + lose sound

--- brachu/Assets/Scripts/DelayButtons.cs
+++ brachu/Assets/Scripts/DelayButtons.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[RequireComponent(typeof(AudioSource))]
 public class DelayButtons : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject go;
     WaitForSecondsRealtime _wait;
+    AudioSource audioSource;
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.pitch = 1.0f;
         _wait = new WaitForSecondsRealtime(3f);
         StartCoroutine(Example());
     }
@@ -18,6 +21,7 @@ public class DelayButtons : MonoBehaviour
         go.SetActive(false);
         yield return _wait;
         print("AA");
+        audioSource.Play();
         go.SetActive(true);
     }
 }
--- brachu/Assets/Scripts/MainMenu.cs
+++ brachu/Assets/Scripts/MainMenu.cs
@@ -17,6 +17,10 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("MainMenu");
     }
+    public void GoTutorial()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
     public void GoToMore()
     {
         SceneManager.LoadScene("More");

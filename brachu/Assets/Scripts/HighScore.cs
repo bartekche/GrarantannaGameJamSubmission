c2ab8f0bb3f59fbc4dbe4b9ccd1d9b4fef3f90e7
Sudoxo <44445493+Sudoxo@users.noreply.github.com>
2020-03-25 15:52:07 +0100

    Add highscore

---
+++ brachu/Assets/Scripts/HighScore.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScore : MonoBehaviour
+{
+    public static int highscore=0;
+    Text highscoretext;
+    void Start()
+    {
+        highscoretext = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Score.score >highscore)
+        { highscore = Score.score; }
+        highscoretext.text = "REKORD: " + highscore.ToString();
+    }
+}

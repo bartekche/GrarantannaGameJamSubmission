d7545726ec9347a1f6b53e0c7a1450e7b3645ebc
bartekche <bartekche@gmail.com>
2020-03-26 00:15:49 +0100

    Added hit sounds in death scene

---
+++ brachu/Assets/Scripts/HitSoundController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(AudioSource))]
+public class HitSoundController : MonoBehaviour
+{
+    WaitForSecondsRealtime _wait;
+    AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.pitch = 1.0f;
+        _wait = new WaitForSecondsRealtime(1f);
+        StartCoroutine(Example());
+
+    }
+
+    IEnumerator Example()
+    {
+        audioSource.Play();
+        yield return _wait;
+        audioSource.Play();
+        yield return _wait;
+        audioSource.Play();
+    }
+}

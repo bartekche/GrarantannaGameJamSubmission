8639329613602cda9074a3f600119d229d8f3597
Sudoxo <44445493+Sudoxo@users.noreply.github.com>
2020-03-25 15:01:40 +0100

    Update DeathScene

---
+++ brachu/Assets/Scripts/DelayButtons.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayButtons : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject go;
+    WaitForSecondsRealtime _wait;
+    void Start()
+    {
+        _wait = new WaitForSecondsRealtime(3f);
+        StartCoroutine(Example());
+    }
+
+    IEnumerator Example()
+    {
+        go.SetActive(false);
+        yield return _wait;
+        print("AA");
+        go.SetActive(true);
+    }
+}

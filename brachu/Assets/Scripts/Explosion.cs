42853983d95b8e4f817bcd4c6d2ed8959005b041
bartekche <bartekche@gmail.com>
2020-03-25 01:49:14 +0100

    Fireball spawner + explosions + background music

---
+++ brachu/Assets/Scripts/Explosion.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public GameObject prefab2;
+    public GameObject prefab;
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        // Put points/ end of game
+
+        if (col.gameObject.tag == "ground")
+        {
+            Destroy(prefab2);
+            Vector3 xyz = transform.position;
+            xyz.y -= 0.25f;
+            GameObject explosion = Instantiate(prefab, xyz, Quaternion.identity);
+          
+            
+        }
+
+
+
+    }
+   
+}
---
+++ brachu/Assets/Scripts/UpSpawn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpSpawn : MonoBehaviour
+{
+    public GameObject prefab;
+    public float spawnRate = 2f;
+    float nextSpawn;
+    // Start is called before the first frame update
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextSpawn)
+        {
+            Vector3 xyz = transform.position;
+            xyz.x = Random.Range(-6.5f, 6.5f);
+            GameObject up = Instantiate(prefab, xyz , transform.rotation);
+            nextSpawn = Time.time + spawnRate;
+        }
+
+    }
+}

749ea0d9c3ea3355d4b2b5d2e47ce6b415dc1799
Sudoxo <44445493+Sudoxo@users.noreply.github.com>
2020-03-26 04:17:03 +0100

    Final comit v2

--- brachu/Assets/Scripts/UpSpawn.cs
+++ brachu/Assets/Scripts/UpSpawn.cs
@@ -9,38 +9,29 @@ public class UpSpawn : MonoBehaviour
     public float spawnRate = 2f;
     public float spawnRate2 = 2f;
     public float spawnRate3 = 2f;
-    float Startingtime;
     float nextSpawn;
     float nextSpawn2;
     float nextSpawn3;
     // Start is called before the first frame update
     // Update is called once per frame
 
-    private void Start()
-    {
-        Startingtime = Time.time;
-        nextSpawn = 1.0f;
-        nextSpawn2 = 2.0f;
-        nextSpawn3 = 3.0f;
-
-    }
     void Update()
     {
-        if (Time.time-Startingtime > nextSpawn)
+        if (Time.time > nextSpawn)
         {
             Vector3 xyz = transform.position;
             xyz.x = player.transform.position.x + Random.Range(-0.5f, 0.5f);
             GameObject up = Instantiate(prefab, xyz , transform.rotation);
             nextSpawn = Time.time + spawnRate;
         }
-        if(Time.time-Startingtime > nextSpawn2)
+        if(Time.time - 1.0f > nextSpawn2)
         {
             Vector3 xyz = transform.position;
             xyz.x = Random.Range(-6.5f, 6.5f);
             GameObject up = Instantiate(prefab, xyz, transform.rotation);
             nextSpawn2 = Time.time + spawnRate2;
         }
-        if (Time.time - Startingtime > nextSpawn3)
+        if (Time.time -2.0f> nextSpawn3)
         {
             Vector3 xyz = transform.position;
             xyz.x = Random.Range(-6.5f, 6.5f);
